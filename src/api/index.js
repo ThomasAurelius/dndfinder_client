fd16bb750363790674bfe70b2ef23cac83a96bf8
Thomas Aurelius <teg824@hotmail.com>
2022-09-03 18:42:56 -0500

    api

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'https://mern-dndfinder.vercel.app' });
+const API = axios.create({ baseURL: 'https://dndfinder.herokuapp.com/' });
 
 API.interceptors.request.use((req) => {
   if (localStorage.getItem('profile')) {
