7830ef6e910cd43890f4abfc13d4db6f7e5143fc
Thomas Aurelius <teg824@hotmail.com>
2022-09-08 17:24:25 -0500

    remove reverse from gridContainer grid

--- src/components/Home/styles.js
+++ src/components/Home/styles.js
@@ -14,7 +14,7 @@ export default makeStyles((theme) => ({
   },
   gridContainer: {
     [theme.breakpoints.down('xs')]: {
-      flexDirection: 'column-reverse',
+      flexDirection: 'column',
     },
   },
   hideForm: {
